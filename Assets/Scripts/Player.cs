fd5f059a4741d8c45d354d9ca1829ec9f1bc3fdf
luhdriloh <007.sam.moore@gmail.com>
2019-02-28 20:56:16 -0700

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+// this delegate references any method with the following signature type
+// result: void
+// parameter 1: int
+// parameter 2 : int
+public delegate void HealthChangeHandler(int maxHealth, int currentHealth);
+
+public class Player : MonoBehaviour
+{
+    // create an event. handler will need to be of same signature as delegate
+    public event HealthChangeHandler _healthChangeEvent;
+    public int _maxHealth;
+    private int _currentHealth;
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(8);
+        }
+    }
+
+    private void TakeDamage(int amountOfDamage)
+    {
+        _currentHealth = Mathf.Max(_currentHealth - amountOfDamage, 0);
+        OnHealthChange();
+
+        if (_currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    // EVENT FUNCTIONS //
+
+    // event is called, call all subscribers 
+    private void OnHealthChange()
+    {
+        if (_healthChangeEvent != null)
+        {
+            _healthChangeEvent(_maxHealth, _currentHealth);
+        }
+    }
+
+    // add a subscriber to the event
+    public void AddHealthSubscriber(HealthChangeHandler healthHandler)
+    {
+        _healthChangeEvent += healthHandler;
+    }
+}
---
+++ Assets/Scripts/PlayerStatHUD.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatHUD : MonoBehaviour
+{
+    public Text _healthText;
+
+    private void Start()
+    {
+        Player player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        player.AddHealthSubscriber(HandleHealthChange);
+    }
+
+    private void HandleHealthChange(int maxHealth, int currentHealth)
+    { 
+        _healthText.text = "Player Health   " + currentHealth + " / " + maxHealth;
+    }
+}
